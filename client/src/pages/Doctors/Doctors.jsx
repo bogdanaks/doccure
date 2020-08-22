e1a9126da51b35d3678c92dd59b574c8f3f98e75
bogdanaks <bogdanaks@bk.ru>
2020-08-22 18:43:23 +0300

    Создание страницы со списком докторов

--- client/src/pages/Doctors/Doctors.jsx
+++ client/src/pages/Doctors/Doctors.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThLarge, faBars } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './styles.module.scss'
+
+import { DocListBlock } from '../../components/DocListBlock/DocListBlock'
 
 export const Doctors = () => {
     return (
-        <div>
-            <h1>Docs page</h1>
+        <div className={styles.doctorsWrapper}>
+            <div className={styles.docHeader}>
+                <h2>2245 Doctors found</h2>
+                <div className={styles.sortBlock}>
+                    <button>
+                        <FontAwesomeIcon icon={faThLarge} />
+                    </button>
+                    <button className={styles.active}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                </div>
+            </div>
+            <div className={styles.docContent}>
+                <DocListBlock />
+                <DocListBlock />
+            </div>
         </div>
     )
 }
