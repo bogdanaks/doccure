e3c9c25decb337e9721d05a4a45c12b82d803ff7
bogdanaks <bogdanaks@bk.ru>
2020-08-22 17:21:04 +0300

    Базовая маршрутизация

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar/Navbar'
+import { Main } from './pages/Main/Main'
+import { Doctors } from './pages/Doctors/Doctors'
 
 function App() {
     return (
         <div className="App">
-            <h1>App</h1>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/" component={Main} exact />
+                    <Route path="/doctors" component={Doctors} exact />
+                </Switch>
+            </Router>
         </div>
     )
 }
