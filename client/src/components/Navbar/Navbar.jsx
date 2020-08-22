cfa8db1f0989b41c56dce66b525204398dea06f0
bogdanaks <bogdanaks@bk.ru>
2020-08-22 16:59:45 +0300

    Базовая стилизация

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+
+import styles from './styles.module.scss'
+import logo from '../../assets/logo.png'
 
 export const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar comp</h1>
+        <div className={styles.navbarWrapper}>
+            <div className={styles.logo}>
+                <img src={logo} alt="Logo" />
+            </div>
+            <ul className={styles.leftBtns}>
+                <li className={styles.active}>Home</li>
+                <li>Doctors</li>
+            </ul>
+            <ul className={styles.rightBtns}>
+                <li className={styles.contactBlock}>
+                    <div className={styles.ico}>
+                        <FontAwesomeIcon icon={faPhoneAlt} />
+                    </div>
+                    <div className={styles.number}>
+                        <span>Contact</span>
+                        <span>+1 315 369 5943</span>
+                    </div>
+                </li>
+                <li>
+                    <button className={styles.loginBtn}>
+                        <span>Login / Signup</span>
+                    </button>
+                </li>
+            </ul>
         </div>
     )
 }
