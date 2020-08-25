2c04e6caa2e5e9dd347371adec5f8b655dcc686e
bogdanaks <bogdanaks@bk.ru>
2020-08-26 00:10:09 +0300

    Создание компонента футер

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+export const Footer = () => {
+    return (
+        <div className={styles.footer}>
+            <h4>Footer</h4>
+        </div>
+    )
+}
