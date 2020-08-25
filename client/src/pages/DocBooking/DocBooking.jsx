36d0cb58cb0b9359ba91a6f20bfac266e0ad5e0c
bogdanaks <bogdanaks@bk.ru>
2020-08-25 23:58:23 +0300

    Правка верстки

--- client/src/pages/DocBooking/DocBooking.jsx
+++ client/src/pages/DocBooking/DocBooking.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './styles.module.scss'
 import doc01 from '../../assets/docs/doc01.jpg'
+
+import { RatingStars } from '../../components/RatingStars/RatingStars'
+import { DocBookingTable } from '../../components/DocBookingTable/DocBookingTable'
 
 export const DocBooking = () => {
     return (
@@ -12,14 +17,21 @@ export const DocBooking = () => {
                         <div className={styles.photo}>
                             <img src={doc01} alt="Doctor" />
                         </div>
+                        <div className={styles.docInfo}>
+                            <h4>Dr. Darren Elder</h4>
+                            <RatingStars isOverall />
+                            <div className={styles.docInfoLocation}>
+                                <FontAwesomeIcon icon={faMapMarkerAlt} />
+                                <span>Newyork, USA</span>
+                            </div>
+                        </div>
                     </div>
-                    <div className={styles.date}></div>
-                    <div className={styles.table}></div>
                 </div>
                 <div className={styles.todayDate}>
                     <h4>11 November 2019</h4>
                     <span>Monday</span>
                 </div>
+                <DocBookingTable />
             </div>
         </div>
     )
