2fe0475d4fdd0bf95554a3bbe05cfb342c971777
bogdanaks <bogdanaks@bk.ru>
2020-08-25 23:22:22 +0300

    Создание компонента

---
+++ client/src/components/DocBookingTable/DocBookingTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+export const DocBookingTable = () => {
+    return (
+        <div>
+            <h1>Doc booking</h1>
+        </div>
+    )
+}
